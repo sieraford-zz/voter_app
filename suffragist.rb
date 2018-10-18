4a73bc1a823c78c8b660fb670a834827920febdc
Siera Ford <sieraford@MACLAP-482HTD6.local>
2018-10-18 09:08:15 -0700

    days 1 and 2 of tutorial

---
+++ suffragist.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+Choices = {
+  'HAM' => 'Hamburger',
+  'PIZ' => 'Pizza',
+  'CUR' => 'Curry',
+  'NOO' => 'Noodles',
+}
+
+get '/' do
+  @title = 'Welcome to the Suffragist!'
+  erb :index
+end
+
+post '/cast' do
+  @title = 'Thanks for casting your vote!'
+  @vote  = params['vote']
+  erb :cast
+end
